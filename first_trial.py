23626666b71ae1d931fbecd85e7f0847c3b74ed2
ycanerol <ycanerol@gmail.com>
2017-01-17 15:25:40 +0100

    Trial python script

---
+++ first_trial.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 17 15:14:50 2017
+
+@author: ycanerol
+
+
+Network simulator in python
+
+Based off of Beggs Plenz 2003 and Beggs Haldemann 2005 papers
+
+
+"""
+
+
+
+
+
