fa296977faf7e4915b165f6563b8d1aedacba324
ycanerol <ycanerol@gmail.com>
2017-01-19 18:43:42 +0100

    Adds in probabilities, fixes number of connections

--- first_trial.py
+++ first_trial.py
@@ -20,11 +20,12 @@ from random import shuffle
 #Initialization
 #Enter and change parameters here
 
-node_nr=200
-transmit_prob=0.01
-avg_connection_per_node=node_nr/10  #To be changed, this is a coarse determination
+node_nr=8
+#avg_connection_per_node=node_nr/10  #To be changed, this is a coarse determination
+connections=4   #Defined as C in the paper
+sigma=1.2
 
-
+#%% Generating the network
 nodes=[[] for i in range(node_nr)]
 #Nodes list will contain lists indices of connected nodes to each element
 
@@ -32,20 +33,37 @@ for i in range(node_nr):
     a=list(range(node_nr))
     shuffle(a)
     #We shuffle the index list, the connections of current node will be taken from here
-    connections=int(np.floor(np.random.normal(avg_connection_per_node,5)))
+    
+    #connections=int(np.floor(np.random.normal(avg_connection_per_node,5)))
     #Number of connections is determined by normal distribution
+    
     nodes[i]=a[:connections+1]
     #We take one more than needed, in case one of the indices point to the neuron itself
+    
     flag=True
     for j in range(connections):
         if nodes[i][j]==i:
             flag=False
-            print("removed {}\n".format(i))
+            #print("removed {}\n".format(i))
             nodes[i].remove(i)
             #Remove self-connection
     if flag:
         del nodes[i][-1]
         #If we removed no self-connections, delete the extra index at the end.
         
-       
-   
+#We established a recurrent network, we need to establish transmission proabability for each connection
+
+tr_probabilities=[[] for i in range(node_nr)]
+for i in range(node_nr):
+    for j in range(len(nodes[i])):
+        divider=list(np.sort(np.random.random_sample(connections-1)))
+        divider.insert(0,0)
+        divider.insert(len(divider),1)
+        divider=np.multiply(divider,sigma) #Scale the interval to be the size of sigma
+        for k in range(1,connections):
+            tr_probabilities[i].append(divider[k]-divider[k-1])
+        #The constraint is that for each node sigma_i should be equal to sigma
+        #Sums of the probabilities for one node should be sigma
+        #For this, we divide the sigma into C parts and use the sizes of the parts as probabilities
+        
+        
