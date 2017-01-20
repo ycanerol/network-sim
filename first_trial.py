79c4f7c5f77d31ecc67d28d0c4ee0ad7c30d12f1
ycanerol <ycanerol@gmail.com>
2017-01-20 16:39:17 +0100

    added time, fixed larger than 1 transmission probabilities

--- first_trial.py
+++ first_trial.py
@@ -20,22 +20,24 @@ from random import shuffle
 #Initialization
 #Enter and change parameters here
 
-node_nr=8
+node_nr=64
 #avg_connection_per_node=node_nr/10  #To be changed, this is a coarse determination
 connections=4   #Defined as C in the paper
 sigma=1.2
 
+#%%
+def set_array(node_nr):     #Function to set array of empty arrays
+    a=[[]for a in range(node_nr)]
+    return a
+
 #%% Generating the network
-nodes=[[] for i in range(node_nr)]
+nodes=set_array(node_nr)
 #Nodes list will contain lists indices of connected nodes to each element
 
 for i in range(node_nr):
     a=list(range(node_nr))
     shuffle(a)
     #We shuffle the index list, the connections of current node will be taken from here
-    
-    #connections=int(np.floor(np.random.normal(avg_connection_per_node,5)))
-    #Number of connections is determined by normal distribution
     
     nodes[i]=a[:connections+1]
     #We take one more than needed, in case one of the indices point to the neuron itself
@@ -52,17 +54,73 @@ for i in range(node_nr):
         #If we removed no self-connections, delete the extra index at the end.
         
 #We established a recurrent network, we need to establish transmission proabability for each connection
+flag=True
+while flag:
+    tr_probabilities=set_array(node_nr)
+    for i in range(node_nr):
+        divider=list(np.sort(np.random.random_sample(connections-1)))
+        divider.insert(0,0)
+        divider.append(1)
+        divider=list(np.multiply(divider,sigma)) #Scale the interval to be the size of sigma
+        for k in range(1,len(divider)):
+            tr_probabilities[i].append(divider[k]-divider[k-1])
+            #The constraint is that for each node sigma_i should be equal to sigma
+            #Sums of the probabilities for one node should be sigma
+            #For this, we divide the sigma into C parts and use the sizes of the parts as probabilities
+    all_less_than1=True
+    for i in range(node_nr):
+        for k in range(connections):
+            if tr_probabilities[i][k]>1:
+                all_less_than1=False
+    if all_less_than1:
+        flag=False
+                       
+del j,k,a,divider,flag
+#The network is set up with required properties at this point
 
-tr_probabilities=[[] for i in range(node_nr)]
-for i in range(node_nr):
-    divider=list(np.sort(np.random.random_sample(connections-1)))
-    divider.insert(0,0)
-    divider.insert(len(divider),1)
-    divider=list(np.multiply(divider,sigma)) #Scale the interval to be the size of sigma
-    for k in range(1,len(divider)):
-        tr_probabilities[i].append(divider[k]-divider[k-1])
-        #The constraint is that for each node sigma_i should be equal to sigma
-        #Sums of the probabilities for one node should be sigma
-        #For this, we divide the sigma into C parts and use the sizes of the parts as probabilities
+#%% Incorporating time and inputs
+time_steps=50 #How many times we transmit
+spont_prob=0.001
+
+def transmit(prob):
+    for i in range(len(prob)):
+        if np.random.random()<prob[i]:
+            return 1
+        else:
+            return 0
+            
+def activate(prob,inputs):
+    pass
+
+outputs=[list(np.zeros(connections)) for i in range(node_nr)]
+
+for t in range(time_steps):
+    
+    inputs=outputs
+    outputs=[list(np.zeros(connections)) for i in range(node_nr)]
+    
+#    for i in range(node_nr):   #Generating a spontaneous input pattern
+#        for k in range(connections):
+#            if np.random.random()<spont_prob:
+#                inputs[i][k]=1
+#            else:
+#                inputs[i][k]=0     #
+
+    for i in range(node_nr):
+        for k in range(connections):
+            if np.random.random()<spont_prob:
+                outputs[i][k]=1
+            elif inputs[i][k]:
+                if np.random.random()<tr_probabilities[i][k]:
+                    outputs[i][k]=1
+            
+            
         
-
+        
+        
+        
+        
+        
+        
+        
+        
--- second_trail_from_linn.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jan 17 15:35:00 2017
-
-@author: zhoulinn
-"""
-
-# the second trail from Linnnn
